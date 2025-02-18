0989a4aba1615ed43ec6bd07252a8e9cd2e4f90a
r.menges <Raphael.Menges@alfatraining.de>
2025-02-18 15:14:53 +0100

    Check for XNNPACK ep being available.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use ort::session::Session;
+use ort::{
+    execution_providers::{ExecutionProvider, XNNPACKExecutionProvider},
+    session::Session,
+};
 
 const YOLOV8M_URL: &str =
     "https://parcel.pyke.io/v2/cdn/assetdelivery/ortrsv2/ex_models/yolov8m.onnx";
 
 fn main() -> anyhow::Result<()> {
-    let builder = Session::builder()?;
+    let mut builder = Session::builder()?;
+
+    let ep = XNNPACKExecutionProvider::default();
+    if !ep.is_available()? {
+        eprintln!("Please compile ONNX Runtime with XNNPACK!");
+        std::process::exit(1);
+    }
+    ep.register(&mut builder)?;
+
     let _session = builder.commit_from_url(YOLOV8M_URL)?;
     Ok(())
 }
