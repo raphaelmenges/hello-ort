bbdff49eba48a5acd012d286eeb98ed5ccff46dd
r.menges <Raphael.Menges@alfatraining.de>
2025-02-18 15:08:42 +0100

    Minimal setup.

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use ort::session::Session;
+
+const YOLOV8M_URL: &str =
+    "https://parcel.pyke.io/v2/cdn/assetdelivery/ortrsv2/ex_models/yolov8m.onnx";
+
+fn main() -> anyhow::Result<()> {
+    let builder = Session::builder()?;
+    let _session = builder.commit_from_url(YOLOV8M_URL)?;
+    Ok(())
+}
