4f0888939df6a95b1e6e6d13c9cbfe6264e93ec5
Raphael Menges <Raphael.Menges@alfatraining.de>
2025-04-04 14:45:50 +0200

    Upgrade `ort`-crate.

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ const YOLOV8M_URL: &str =
 fn main() -> anyhow::Result<()> {
     let builder = Session::builder()?;
     let _session = builder.commit_from_url(YOLOV8M_URL)?;
+    println!("{}", "This did not crash, good!");
     Ok(())
 }
