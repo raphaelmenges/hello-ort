96b72232f43a6f9cdf370ac21f9f26e7230cc1eb
Raphael Menges <Raphael.Menges@alfatraining.de>
2025-04-07 10:00:32 +0200

    Automatically download OpenVINO libraries.

--- build.rs
+++ build.rs
@@ -1,48 +1,86 @@
 use reqwest::blocking::get;
 use std::{
-    fs::{File, exists, remove_file},
+    fs::{File, create_dir_all, exists, remove_file},
     io::{Cursor, Read, Write},
 };
 use zip::ZipArchive;
 
 static ORT_ARTIFACT_URL: &str = "https://github.com/alfatraining/ort-artifacts-staging/releases/download/v1.21.0-openvino-windows/ort-rel-1.21.0-x86_64-pc-windows-msvc-md.zip";
+static OPENVINO_DISTRIBUTION_URL: &str = "https://storage.openvinotoolkit.org/repositories/openvino/packages/2025.0/windows/openvino_toolkit_windows_2025.0.0.17942.1f68be9f594_x86_64.zip";
 
-fn main() {
-    #[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos"))]
-    println!("cargo:rustc-link-arg=-fapple-link-rtlib");
+fn fetch_and_extract(url: String, files: Vec<(String, String)>) {
+    let mut request = get(url).expect("Cannot request archive.");
 
-    // Request archive.
-    let mut request = get(ORT_ARTIFACT_URL).expect("Cannot request precompiled onnxruntime.");
     let mut buf = Vec::<u8>::new();
-    request
-        .read_to_end(&mut buf)
-        .expect("Cannot read precompiled onnxruntime.");
+    request.read_to_end(&mut buf).expect("Cannot read archive.");
 
-    // Prepare extraction.
     let reader = Cursor::new(buf);
     let mut zip = ZipArchive::new(reader).expect("Cannot incept unzipper.");
 
-    // Extract precompiled library.
-    {
-        let mut extract = |filename| {
-            let mut buf = Vec::<u8>::new();
-            zip.by_name(format!("onnxruntime/lib/{}", filename).as_str())
-                .expect("Cannot find file in zip.")
-                .read_to_end(&mut buf)
-                .expect("Cannot read file in zip.");
+    for (path, name) in files {
+        let mut buf = Vec::<u8>::new();
+        zip.by_name(format!("{}/{}", path, name).as_str())
+            .expect("Cannot find file in zip.")
+            .read_to_end(&mut buf)
+            .expect("Cannot read file in zip.");
 
-            let out_path = format!("./{}", filename);
-            if exists(&out_path).expect("Cannot check file on disk.") {
-                remove_file(&out_path).expect("Cannot delete file on disk.");
-            }
-            File::create(&out_path)
-                .expect("Cannot create file.")
-                .write_all(&buf)
-                .expect("Cannot fill file.");
-        };
+        File::create(format!("./{}", name))
+            .expect("Cannot create file.")
+            .write_all(&buf)
+            .expect("Cannot write file.");
 
-        extract("onnxruntime.lib");
-        extract("onnxruntime_providers_openvino.dll");
-        extract("onnxruntime_providers_shared.dll");
+        // Already put .dll's into binary folder.
+        if name.ends_with(".dll") {
+            let create_next_to_binary = |target| {
+                create_dir_all(target).expect("Cannot create target dir.");
+                File::create(format!("{target}/{name}"))
+                    .expect("Cannot create file.")
+                    .write_all(&buf)
+                    .expect("Cannot write file.");
+            };
+
+            create_next_to_binary("./target/debug");
+            create_next_to_binary("./target/release");
+        }
     }
+}
+
+fn main() {
+    // Prepare ONNX runtime.
+    fetch_and_extract(
+        String::from(ORT_ARTIFACT_URL),
+        vec![
+            (
+                String::from("onnxruntime/lib"),
+                String::from("onnxruntime.lib"),
+            ),
+            (
+                String::from("onnxruntime/lib"),
+                String::from("onnxruntime_providers_openvino.dll"),
+            ),
+            (
+                String::from("onnxruntime/lib"),
+                String::from("onnxruntime_providers_shared.dll"),
+            ),
+        ],
+    );
+
+    // Prepare OpenVINO.
+    fetch_and_extract(
+        String::from(OPENVINO_DISTRIBUTION_URL),
+        vec![
+            (
+                String::from(
+                    "openvino_toolkit_windows_2025.0.0.17942.1f68be9f594_x86_64/runtime/bin/intel64/Release",
+                ),
+                String::from("openvino.dll"),
+            ),
+            (
+                String::from(
+                    "openvino_toolkit_windows_2025.0.0.17942.1f68be9f594_x86_64/runtime/3rdparty/tbb/bin",
+                ),
+                String::from("tbb12.dll"),
+            ),
+        ],
+    );
 }
