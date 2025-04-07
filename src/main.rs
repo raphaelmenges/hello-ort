a04129056bf8e782ec3b680fd705a4afe1d37a76
Raphael Menges <raphael.menges@alfatraining.de>
2025-04-07 10:30:43 +0200

    Add feedback.

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,11 @@ fn main() -> anyhow::Result<()> {
 
     let mut builder = Session::builder()?;
     let ep = OpenVINOExecutionProvider::default();
-    println!("{}", ep.is_available()?);
+    println!("OpenVINO ep available: {}", ep.is_available()?);
     ep.register(&mut builder)?;
+    println!("OpenVINO ep registered!");
     let mut session = builder.commit_from_url(YOLOV8M_URL)?;
+    println!("Session built!");
     let input: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>> = Array::zeros((1, 3, 640, 640));
     let _outputs = session.run(inputs!["images" => TensorRef::from_array_view(&input)?])?;
     println!("{}", "This did not crash, good!");
